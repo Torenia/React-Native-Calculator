fb4788b2e97881974a318aa587e6c064a995d3d9
Osadcha, Tetiana <tetiana.osadcha@mckesson.com>
2021-10-19 12:23:52 +0300

    React Native Calculator

---
+++ button.component.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Text, TouchableOpacity } from 'react-native';
+import { colors } from './constants';
+
+export const CalcBtn = ({ onPress, styleContainer, disabled, title }) => {
+  const _style = disabled
+    ? {
+      ...styleContainer,
+      backgroundColor: colors.GREY,
+    }
+    : styleContainer;
+
+  return (
+    <TouchableOpacity
+      onPress={disabled ? () => { } : onPress}
+      style={_style}
+      activeOpacity={disabled ? 1 : 0.5}
+    >
+      <Text>{title}</Text>
+    </TouchableOpacity>
+  );
+}
---
+++ constants.js
@@ -0,0 +1,10 @@
+export const colors = {
+  RED: 'red',
+  BLACK: 'black',
+  GREY: 'grey',
+  CONTAINER_MAIN: '#ecf0f1',
+  INPUT_BACKGROUND: '#e6f6ff',
+  BUTTON_BG: '#fbfff3',
+  YELLOW: '#fff9c0',
+  BLUE: '#83cbff'
+};
